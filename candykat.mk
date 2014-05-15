f38b6bc1e717e57718c97949e2ea5721c4133407
errantzebra845 <errantzebra845@gmail.com>
2014-05-15 13:55:13 -0400

    make it a candy device

--- candykat.mk
+++ candykat.mk
@@ -3,10 +3,10 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/slim/config/gsm.mk)
+$(call inherit-product, vendor/candykat/config/gsm.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/candykat/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/maguro/full_maguro.mk)
@@ -20,7 +20,7 @@ PRODUCT_RELEASE_NAME := maguro
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.2.2/JDQ39/573038:user/release-keys"
 PRIVATE_BUILD_DESC="yakju-user 4.2.2 JDQ39 573038 release-keys"
 
-PRODUCT_NAME := slim_maguro
+PRODUCT_NAME := candykat_maguro
 PRODUCT_DEVICE := maguro
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Galaxy Nexus
